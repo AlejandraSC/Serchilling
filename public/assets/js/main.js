71070c54fcb016c8e15a44dd19f953b695cb5d02
AlejandraSC <alejandrasalmeron6@gmail.com>
2020-10-24 10:43:50 +0200

    Create containers with DOM advanced  and function to show user searched serie

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,30 +5,29 @@ const request = document.querySelector('.js-inputText');
 const results = document.querySelector('.results__container');
 
 //Recoger lo que ha escrito el usuario y PINTAR SERIE BUSCADA (PASARLO A UNA FUNCIÓN NUEVA PARA QUE QUEDE MÁS LIMPIOFunction paintRequest)
-
 function getData() {
   const userValue = request.value;
   fetch('//api.tvmaze.com/search/shows?q=' + userValue)
     .then((response) => response.json())
     .then((series) => {
+      console.log(series);
       for (let serie of series) {
         //Titulo serie
         const resultTitle__paragr = document.createElement('p');
         const resultTitle__content = document.createTextNode(serie.show.name);
         resultTitle__paragr.appendChild(resultTitle__content);
-        console.log(resultTitle__paragr);
-
         //foto serie
         const resultImg__container = document.createElement('img');
         resultImg__container.classList.add('resultImg');
         resultImg__container.classList.add('js-resultImg');
-        resultImg__container.src = serie.show.image.original;
-        console.log(resultImg__container);
-
+        //seleccionamos la imagen, si no existe creamos una con el nombre
+        if (serie.show.image) {
+          resultImg__container.src = serie.show.image.original;
+        } else {
+          resultImg__container.src = '//via.placeholder.com/210x295/ffffff/666666/?text=' + serie.show.name;
+        }
         // Incluir en div
         includeInMyDiv(resultTitle__paragr, resultImg__container);
-
-        listenImages();
       }
     });
 }
@@ -41,41 +40,7 @@ function includeInMyDiv(title, img) {
   results.appendChild(eachSerie__container);
 }
 
-function listenImages() {
-  const seriesImg = document.querySelectorAll('.js-resultImg');
-  for (const serieImg of seriesImg) {
-    serieImg.addEventListener('click', favSeries);
-  }
-}
-
-//SERIES FAVORITAS
-/*function favSeries(ev) {
-  const fav = document.querySelector('.fav__container');
-  console.log('escucho evento');
-  console.log(ev.path[0].src);
-  const favSerie = document.createElement('img');
-  favSerie.src = ev.path[0].src;
-  fav.appendChild(favSerie);
-}
-*/
-
-function favSeries(ev) {
-  const fav = document.querySelector('.fav__container');
-  console.log('escucho evento');
-}
-
 searchButton.addEventListener('click', getData);
-
-/*
-resultImg__container.addEventListener('click', favourites);
-
-1. Escuchar el evento click de cada imágen 
-2. Identificar el elemento clicado
-3. guardar en un Array cuales son las favoritas 
-4. Añadir la clase de favorito (Borde cambia a rojo)
-5. Quitar la clase de favorito
-
-*/
 
 //Borrar antes de subir
 searchButton.click();
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const searchButton = document.querySelector('.js-search__btn');
+const request = document.querySelector('.js-inputText');
+const results = document.querySelector('.results__container');
+
+//Recoger lo que ha escrito el usuario y PINTAR SERIE BUSCADA (PASARLO A UNA FUNCIÓN NUEVA PARA QUE QUEDE MÁS LIMPIOFunction paintRequest)
+function getData() {
+  const userValue = request.value;
+  fetch('//api.tvmaze.com/search/shows?q=' + userValue)
+    .then((response) => response.json())
+    .then((series) => {
+      console.log(series);
+      for (let serie of series) {
+        //Titulo serie
+        const resultTitle__paragr = document.createElement('p');
+        const resultTitle__content = document.createTextNode(serie.show.name);
+        resultTitle__paragr.appendChild(resultTitle__content);
+        //foto serie
+        const resultImg__container = document.createElement('img');
+        resultImg__container.classList.add('resultImg');
+        resultImg__container.classList.add('js-resultImg');
+        //seleccionamos la imagen, si no existe creamos una con el nombre
+        if (serie.show.image) {
+          resultImg__container.src = serie.show.image.original;
+        } else {
+          resultImg__container.src = '//via.placeholder.com/210x295/ffffff/666666/?text=' + serie.show.name;
+        }
+        // Incluir en div
+        includeInMyDiv(resultTitle__paragr, resultImg__container);
+      }
+    });
+}
+
+function includeInMyDiv(title, img) {
+  const eachSerie__container = document.createElement('div');
+  eachSerie__container.classList.add('eachSerie__container');
+  eachSerie__container.appendChild(title);
+  eachSerie__container.appendChild(img);
+  results.appendChild(eachSerie__container);
+}
+
+searchButton.addEventListener('click', getData);
+
+//Borrar antes de subir
+searchButton.click();
